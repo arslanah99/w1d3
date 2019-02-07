f13910b5c3f6b12d6d3bcfc02e6befbcdd8b33da
arslan <arah09@hotmail.com>
2019-02-06 17:16:00 -0700

    Commited

--- characterCount.js
+++ characterCount.js
@@ -1,4 +1,15 @@
-function countLetters(char, instance){
-    //returns all unique characters that existin a string in the function
-    
+function countLetters(string){
+    string = string.replace(/\s/g, '');
+ var charCounts = {};
+    for(var i = 0; i < string.length; i++){
+        var curChar = string[i];
+        if (charCounts[curChar]) {
+            charCounts[curChar] += 1;
+        } else {
+            charCounts[curChar] = 1;
+        }
+    }
+    return charCounts;
 }
+
+console.log(JSON.stringify(countLetters('lighthouse in the house'), null, 2))
